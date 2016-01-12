300b90f106e06cb2f4980567224f1a4de1173207
Renrhaf <quentin.fahrner@actency.fr>
2016-01-12 23:12:33 +0100

    push libs and menu/footer theming

---
+++ web/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+    <!-- ================================================== -->
+    <!-- =============== START JS OPTIONS ================ -->
+    <!-- ================================================== -->
+
+    $(document).ready(function () {
+        // Go to top button
+        $(window).load(function () {
+            $(window).scroll(function () {
+                if ($(document).scrollTop() > 300) {
+                    $('#back-to-top').css({bottom: "50px"});
+                }
+                else {
+                    $('#back-to-top').css({bottom: "-80px"});
+                }
+            });
+        });
+
+        $('#back-to-top').on("click", function () {
+            $('html, body').animate({scrollTop: 0}, 'slow');
+            return false;
+        });
+    });
+})(jQuery);
